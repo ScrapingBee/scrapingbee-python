0748a7c5f2189f3e0852ad65e3db9a1a0e9f9ead
arimbr <arimbr@gmail.com>
2021-05-19 09:04:07 +0200

    Add type annotations to client.py

--- scrapingbee/client.py
+++ scrapingbee/client.py
@@ -1,4 +1,4 @@
-from requests import request
+from requests import request, Response
 
 from .utils import get_scrapingbee_url, process_headers
 
@@ -6,10 +6,12 @@ from .utils import get_scrapingbee_url, process_headers
 class ScrapingBeeClient:
     api_url = 'https://app.scrapingbee.com/api/v1/'
 
-    def __init__(self, api_key):
+    def __init__(self, api_key: str):
         self.api_key = api_key
 
-    def request(self, method, url, params=None, data=None, headers=None, cookies=None, **kwargs):
+    def request(self, method: str, url: str, params: dict = None, data: dict = None,
+                headers: dict = None, cookies: dict = None, **kwargs
+                ) -> Response:
         if not params:
             params = {}
 
@@ -29,10 +31,14 @@ class ScrapingBeeClient:
 
         return request(method, spb_url, data=data, headers=headers, **kwargs)
 
-    def get(self, url, params=None, headers=None, cookies=None, **kwargs):
+    def get(self, url: str, params: dict = None, headers: dict = None, cookies: dict = None,
+            **kwargs
+            ) -> Response:
         return self.request('GET', url, params=params, headers=headers, cookies=cookies, **kwargs)
 
-    def post(self, url, params=None, data=None, headers=None, cookies=None, **kwargs):
+    def post(self, url: str, params: dict = None, data: dict = None, headers: dict = None,
+             cookies: dict = None, **kwargs
+             ) -> Response:
         return self.request(
             'POST',
             url,
