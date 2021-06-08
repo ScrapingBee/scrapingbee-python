d346faf2c4e7a230224c5f098c577fd73f29a7aa
Pierre de Wulf <pierredewulf31@gmail.com>
2021-06-08 21:05:20 +0200

    Update to 1.1.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='scrapingbee',
-    version='1.1.2',
+    version='1.1.3',
     url='https://github.com/scrapingbee/scrapingbee-python',
     description='ScrapingBee Python SDK',
     long_description=open('README.md').read(),
